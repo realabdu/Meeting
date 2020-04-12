5e6febff49581c03d3210c0a35c77f07a7b3d24d
polysoulz <amosibah@edanswers.com>
2020-04-12 16:32:03 +0300

    idk

--- coetus/room/views.py
+++ coetus/room/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt, xframe_options_sameorigin
+
 from .models import room , file
+
 
 @login_required
 def demo(request):
-    return render(request=request , template_name='pages/demo.html' )
+    qs_f = file.objects.all()
+    return render(request=request , template_name='pages/demo.html', context={'files':qs_f}  )
 
 def landing(request):
 
-    qs_f=  file.objects.all()
-    return render(request=request , template_name='pages/landing.html' , context={'files':qs_f} )
+    return render(request=request , template_name='pages/landing.html' )
 
 
 # Create your views here.
